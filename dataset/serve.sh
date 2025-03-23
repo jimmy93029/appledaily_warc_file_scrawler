da0438d2439016511d2fb63b3852fc404dc40174
we <jimmywu0229@gmail.com>
2025-03-23 01:48:16 +0000

    sort files for building

--- dataset/serve.sh
+++ dataset/serve.sh
@@ -1,7 +1,10 @@
-datasette /mnt/data/text.db -p 8211 \
-    --reload \
+#!/bin/bash
+
+datasette /mnt/data/text.db \
+    -h 0.0.0.0 -p 8081 --cors --reload \
     --template-dir=templates/ \
     --plugins-dir=plugins/ \
     --static images:/mnt/data/appledaily \
-    --static static:/home/jimmywu/sinica/venv/codes/museums/static \
+    --static static:/home/jimmywu/sinica/venv/gitspace/dataset/static \
     -m metadata.yaml
+
